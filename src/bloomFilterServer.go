5659629ab3e6c3d1743bf131180e464a89e9564b
vlam321 <vlam321@gmail.com>
2017-11-09 02:00:20 -0800

    Added bloomFilterServer.go

---
+++ src/bloomFilterServer.go
@@ -0,0 +1,11 @@
+/* This file will contain functionalities to host the main service
+in which the client will make requests to. Use net/http to create
+API endpoints to access the following functionalities:
+	- updateBitArray
+	- checkMembership
+	- etc. (@Grace and Steph
+		add more functionalities
+		here and implement this
+		after you've completed your
+		current tasks)
+*/
