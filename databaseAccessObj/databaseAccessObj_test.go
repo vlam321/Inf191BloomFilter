3c41e9abb98e2ef096379b04c1f103f8bd8e71df
Jonathan Chew <jchew1@uci.edu>
2017-11-17 00:30:38 -0800

    Updates to DAO

--- databaseAccessObj/databaseAccessObj_test.go
+++ databaseAccessObj/databaseAccessObj_test.go
@@ -15,7 +15,7 @@ package databaseAccessObj
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
-	"Inf191BloomFilter/src/bloomDataGenerator"
+	"Inf191BloomFilter/bloomDataGenerator"
 )
 
 const dsn = "bloom:test@/unsubscribed"
@@ -31,31 +31,6 @@ func TestHasTable(t *testing.T){
 	assert.False(t, update.hasTable("unsubscribed", "nothere"))
 	assert.False(t, update.hasTable("unsubscribed", "nothereeither"))
 	assert.False(t, update.hasTable("unsubscribed", "nope"))
-	update.CloseConnection()
-}
-
-func TestEnsureTable(t *testing.T){
-	update := New(dsn)
-	// Test ensure table on a table that does exist to make sure
-	// there's no bugs
-	assert.True(t, update.hasTable("unsubscribed", "unsub_0"))
-
-	// Delete these tables if they exist in the db
-	update.dropTable("unsub_99")
-	update.dropTable("unsub_100")
-
-	// Make sure that these table doesn't exist in the databse
-	assert.False(t, update.hasTable("unsubscribed", "unsub_1"))
-	assert.False(t, update.hasTable("unsubscribed", "unsub_2"))
-
-	// Run ensure table using the same inputs to create the tables
-	update.EnsureTable("unsub_99")
-	update.EnsureTable("unsub_100")
-
-	// double check to make sure that the tables are created
-	assert.True(t, update.hasTable("unsubscribed", "unsub_1"))
-	assert.True(t, update.hasTable("unsubscribed", "unsub_2"))
-
 	update.CloseConnection()
 }
 
