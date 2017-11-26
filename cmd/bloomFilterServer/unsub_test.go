1c4691c637a20e06b044f7cbec3b5c16bca000dc
vlam321 <vlam321@gmail.com>
2017-11-25 17:35:48 -0800

    Added test for resubscription in unsub_test

--- cmd/bloomFilterServer/unsub_test.go
+++ cmd/bloomFilterServer/unsub_test.go
@@ -150,5 +150,66 @@ func TestNewUnsubscribes(t *testing.T) {
 	// Check again to see if bit array is updated
 	fmt.Printf("%d == %d\n", len(arr2.Trues), len(dataSet[0]))
 	assert.True(t, len(arr2.Trues) == len(dataSet[0]))
+}
+
+func TestResubscribed(t *testing.T) {
+	dao := databaseAccessObj.New(dsn)
+
+	// Clear tables for clean tests
+	dao.Clear()
+
+	/*
+		1. use dao to grab some data pairs, store in a var
+		2. use dao to remove these pairs from the db
+		3. call BF server to update the bit array
+		4. Run the saved pairs against the BF server and make sure the get an empty array
+	*/
+
+	var allData []string
+	dataSet := bloomDataGenerator.GenData(1, 10, 20)
+	extra := bloomDataGenerator.GenData(1, 10, 20)
+
+	allData = append(allData, dataSet[0]...)
+	allData = append(allData, extra[0]...)
+
+	dataSum := make(map[int][]string)
+	dataSum[0] = allData
+
+	dao.Insert(dataSum)
+
+	_, err := http.Get(updateEndpoint)
+	checkErr(err)
+
+	// Remove the extra from the database
+	dao.Delete(extra)
+
+	// update the bloom filter bit array again
+	_, err = http.Get(updateEndpoint)
+	checkErr(err)
+
+	buff := new(bytes.Buffer)
+	payload := Payload{0, dataSum[0]}
+
+	data, err := json.Marshal(payload)
+	checkErr(err)
+
+	err = json.NewEncoder(buff).Encode(data)
+	checkErr(err)
+
+	// get the memberships using yall the data
+	res, _ := http.Post(membershipEndpoint, "application/json; charset=utf-8", buff)
+
+	body, err := ioutil.ReadAll(res.Body)
+	checkErr(err)
+
+	var arr Result
+	// converts the decoded result back to a Result struct
+	err = json.Unmarshal(body, &arr)
+	checkErr(err)
+
+	fmt.Printf("%d != %d", len(dataSum[0]), len(arr.Trues))
+	assert.False(t, len(dataSum[0]) == len(arr.Trues))
+
+	// Grab some of these data from the database
 
 }
