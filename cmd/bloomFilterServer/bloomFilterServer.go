ab23f8d446b01021de8b7e0f636c60295240e53a
mondrags <mondrags@uci.edu>
2017-11-28 06:08:08 -0800

    added incomplete ticker for conitnuous bloom filter updating

--- cmd/bloomFilterServer/bloomFilterServer.go
+++ cmd/bloomFilterServer/bloomFilterServer.go
@@ -2,13 +2,9 @@
 in which the client will make requests to. Use net/http to create
 API endpoints to access the following functionalities:
 	- UpdateBloomFilter
-	- RepopulateBloomFilter
+		- AddToBloomFilter
+		- RepopulateBloomFilter
 	- GetArrayOfUnsubscribedEmails
-	- etc. (Steph
-		add more functionalities
-		here and implement this
-		after you've completed your
-		current tasks)
 */
 package main
 
@@ -17,10 +13,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strconv"
 )
 
-type Payload struct {
+type payload struct {
 	UserId int
 	Emails []string
 }
@@ -29,53 +24,93 @@ type Payload struct {
 //The bloom filter for this server
 var bf = bloomManager.New()
 
+
 //handleUpdate will update the respective bloomFilter
-func handleUpdate(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Received request: %v %v %v\n", r.Method, r.URL, r.Proto)
-	bf.UpdateBloomFilter()
+//server will keep track of when the last updated time is. to call 
+//update every _ time. 
+// func handleUpdate(r http.ResponseWriter, req *http.Request) {
+// 	if lastUpdate.Sub(time.Now()).Seconds == time.Minute{
 
-}
+// 	}
+// 	bf.UpdateBloomFilter()
 
-//checkErr checks fro errors in the decoded text and encoded text...
+// }
+
+//checkErr checks fro errors in the decoded text and encoded text... 
 func checkErr(err error) {
 	if err != nil {
 		panic(err.Error())
 	}
 }
 
+//handleFilterUnsubscripe handles when the client requests to recieve 
+//those emails that are unsubscribed therefore, IN the database. 
 func handleFilterUnsubscribed(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Received request: %v %v %v\n", r.Method, r.URL, r.Proto)
+	
 	var buff []byte
-	var payload Payload
-	//Result struct made to carry the result of unsuscribed emails
+	var payload payload
+
+	//Result struct made to carry the result of unsuscribed emails 
 	type Result struct {
-		Trues []string
+	Trues []string
 	}
+
+	//decode byets from request body
 	err := json.NewDecoder(r.Body).Decode(&buff)
+
+	//check for error; if error write 404
 	if err != nil {
-		http.Error(w, err.Error(), 400)
-		return
+		w.WriteHeader(404)
+		w.Write([]byte("404 - " + http.StatusText(404)))
+		// http.Error(w, err.Error(), 400)
+		// return
 	}
+	
+	// converts the decoded result to a payload struct
 	err = json.Unmarshal(buff, &payload)
 	checkErr(err)
 
-	var emailInputs []string
-	for i := range payload.Emails {
-		emailInputs = append(emailInputs, strconv.Itoa(payload.UserId)+"_"+payload.Emails[i])
-	}
+	fmt.Println(payload)
 
-	emails := bf.GetArrayOfUnsubscribedEmails(emailInputs)
+	//uses bloomManager to get the result of unsubscribed emails 
+	//puts them in struct, result 
+	emails := bf.GetArrayOfUnsubscribedEmails(payload.Emails)
 	filteredEmails := Result{emails}
 
+	//convert result to json 
 	js, err := json.Marshal(filteredEmails)
 	checkErr(err)
 
+	fmt.Println(js)
+
+	//write back to client
 	w.Write(js)
 }
 
-func main() {
-	http.HandleFunc("/filterUnsubscribed", handleFilterUnsubscribed)
-	http.HandleFunc("/update", handleUpdate)
 
+func timeManager(){
+	ticker := time.NewTicker(time.Second * 2)	
+	go func() {
+        	for t := range ticker.C {
+				//Call update bloom filter
+				bf.UpdateBloomFilter()
+            	fmt.Println("Bloom Filter Updated at: ", t.Format("2006-01-02 3:4:5 PM"))
+	
+       		}
+	}()
+	//Figure out how to run without sleep? 
+	//use go forever? an option
+	time.Sleep(time.Second * 10)
+    	ticker.Stop()
+    	fmt.Println("Ticker stopped")
+}
+
+
+func main() {
+   
+	timeManager()
+	http.HandleFunc("/filterUnsubscribed", handleFilterUnsubscribed)
+	// http.HandleFunc("/update", handleUpdate)
+	
 	http.ListenAndServe(":9090", nil)
 }
