a96c8d764ef6491d04d4e96979ad519d72dcd775
jchew1 <jchew513@gmail.com>
2018-02-26 01:51:35 -0800

    bloomRouterBench updated

--- cmd/bloomBench/bloomRouterBench/bloomRouterBench.go
+++ cmd/bloomBench/bloomRouterBench/bloomRouterBench.go
@@ -44,23 +44,66 @@ func getUnsub(dataset map[int][]string) map[int][]string {
 			log.Printf("Error unmarshaling body: %v\n", err)
 			return nil
 		}
+		result[temp.UserId] = temp.Emails
 	}
 	return result
 }
+
+// skip marshal/unmarshal
+/*
+func getUnsub(pl [][]byte) [][]byte{
+	var result [][]byte
+	for i:=0; i<len(pl); i++{
+		res, err := http.Post(awsEp, "application/json; charset=utf-8", bytes.NewBuffer(pl[i]))
+		if err != nil{
+			log.Printf("Error processing request: %v\n", err)
+			return nil
+		}
+		defer res.Body.Close()
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil{
+			log.Printf("Error reading body: %v\n", err)
+			return nil
+		}
+		result = append(result, body)
+	}
+	return result
+}
+*/
 
 func benchmarkUnsub(trueResults, falseResults int, b *testing.B) {
 	falseResults = 0
 	dao := databaseAccessObj.New()
 	defer dao.CloseConnection()
 	testData := make(map[int][]string)
+
+	// skip marshal/unmarshal
+	/*
+	var pl []payload.Payload
+	var plJson [][]byte
+	*/
+
 	for i := 0; i < numShards; i++ {
 		testData[i] = append(dao.SelectRandSubset(i, trueResults)[i], bloomDataGenerator.GenData(1, falseResults, falseResults+1)[0]...)
+		// skip marshal/unmarshal
+		/*
+		pl = append(pl, payload.Payload{i, testData[i]})
+		tempJson, err := json.Marshal(pl[i])
+		if err != nil{
+			log.Printf("error marshaling: %v\n", err)
+			return
+		}
+		plJson = append(plJson, tempJson)
+		*/
 	}
+
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		getUnsub(testData)
+		//getUnsub(plJson)
 	}
 	b.StopTimer()
+
 	log.Printf("Ran unsubscribed members filter benchmark: %d shards %d true values and %d false values (%d iterations).\n", numShards, trueResults, falseResults, b.N)
 }
 
@@ -80,18 +123,20 @@ func BenchmarkUnsub8000(b *testing.B)   { benchmarkUnsub(8000, 8000, b) }
 func BenchmarkUnsub16000(b *testing.B)  { benchmarkUnsub(16000, 16000, b) }
 func BenchmarkUnsub50000(b *testing.B)  { benchmarkUnsub(50000, 50000, b) }
 func BenchmarkUnsub100000(b *testing.B) { benchmarkUnsub(100000, 100000, b) }
+func BenchmarkUnsub500000(b *testing.B) { benchmarkUnsub(500000, 500000, b) }
+func BenchmarkUnsub1000000(b *testing.B) { benchmarkUnsub(1000000, 1000000, b) }
 
 func main() {
 	// go run cmd/bloomBench/bloomRouterBench/bloomRouterBench.go [number of working db shards]
 	log.Printf("starting bloom router bench\n")
 	numShards, _ = strconv.Atoi(os.Args[1])
-
 	f, err := os.Create(fmt.Sprintf("log/bench%d.txt", numShards))
 	if err != nil {
 		log.Printf("Error creating file: %v\n", err)
 		return
 	}
 	defer f.Close()
+
 	_, err = f.WriteString(fmt.Sprintf("numShards: %d\n", numShards))
 	if err != nil {
 		log.Printf("Error writing numShards: %v\n", err)
@@ -119,4 +164,10 @@ func main() {
 	res = testing.Benchmark(BenchmarkUnsub100000)
 	log.Println(res.NsPerOp())
 	logResult(f, 100000, res.NsPerOp())
+	res = testing.Benchmark(BenchmarkUnsub500000)
+	log.Println(res.NsPerOp())
+	logResult(f, 500000, res.NsPerOp())
+	res = testing.Benchmark(BenchmarkUnsub1000000)
+	log.Println(res.NsPerOp())
+	logResult(f, 1000000, res.NsPerOp())
 }
